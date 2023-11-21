45a0252971299f9b7db70ceae47eeb0410cb749d
James-Owolabi Olaoluwa David <jamesowolabiola1@gmail.com>
2023-11-21 11:07:16 -0600

    PythonApplication2.py
    Added a functional sql connector and the dotenv library to store information

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -1,95 +1,128 @@
+import os
 import mysql.connector
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
 class FileManagement:
     def __init__(self):
-        self.mydb = mysql.connector.connect(
-            host = 'localhost',
-            user = 'root',
-            password = 'olajames',
-            database = 'finalproject'
-        )
-        self.cursor = self.mydb.cursor()
-    
+        self.mydb = None
+        self.cursor = None
+        try:
+            self.mydb = mysql.connector.connect(
+                host='localhost',  # host parameter
+                user='root',
+                password='Supernatural_2006',
+                database='file_management_systemsdb'
+            )
+            self.cursor = self.mydb.cursor()
+            print("Database connection established.")  # Logging for successful connection
+        except mysql.connector.Error as err:
+            print(f"Database Connection Error: {err}")
+            if self.mydb is not None:
+                self.mydb.close()  # Close connection if partially established
+
+
     def accept_fileinformation(self):
-        fileID = int(input('Enter FileID: '))
-        filename = input('Enter File Name: ')
-        filesize = input('Enter file size in MB: ')
-        lastmodifieddate = input('Enter last modified date [format(Year-Month-Date): ]: ')
-        lastaccesseddate = input('Enter last modified date [format(Year-Month-Date): ]: ')
-        filetype = input('Enter file type : ')
-        
-        insert_query = """
-                INSERT INTO filemanagement(fileID,filename,filesize,lastmodifieddate,lastaccesseddate,filetype) VALUES
-                (%s,%s,%s,%s,%s,%s)
+        try:
+            fileID = int(input('Enter FileID: '))
+            filename = input('Enter File Name: ')
+            filesize = float(input('Enter file size in MB: '))
+            lastmodifieddate = input('Enter last modified date [YYYY-MM-DD]: ')
+            lastaccesseddate = input('Enter last accessed date [YYYY-MM-DD]: ')
+            filetype = input('Enter file type: ')
+
+            insert_query = """
+                INSERT INTO filemanagement (fileID, filename, filesize, lastmodifieddate, lastaccesseddate, filetype)
+                VALUES (%s, %s, %s, %s, %s, %s)
             """
-        print(f'Property with ID {fileID} has been added.')
-              
+            self.cursor.execute(insert_query, (fileID, filename, filesize, lastmodifieddate, lastaccesseddate, filetype))
+            self.mydb.commit()
+            print(f'File with ID {fileID} has been added.')
+        except mysql.connector.Error as err:
+            print(f"SQL Error: {err}")
+
     def search_fileinformation(self):
-        fileID = input('Enter File ID to search: ')
-        search_query = f'SELECT * from filemanagement where fileID = %s'
-        self.cursor.execute(search_query,(fileID,))
-        found = self.cursor.fetchall()
-        
-        if found:
-            print('File Found')
-            for x in found:
-                print(x)
-        else:
-            print('No matching file')
-            
+        try:
+            fileID = int(input('Enter File ID to search: '))
+            search_query = 'SELECT * FROM filemanagement WHERE fileID = %s'
+            self.cursor.execute(search_query, (fileID,))
+            found = self.cursor.fetchall()
+
+            if found:
+                print('File Found:')
+                for x in found:
+                    print(x)
+            else:
+                print('No matching file')
+        except mysql.connector.Error as err:
+            print(f"SQL Error: {err}")
+
     def update_fileinformation(self):
-        fileID = input('Enter file ID of what to update: ')
-        column = input('Enter column to update its value')
-        newvalue = input('Enter the new value: ')
-        update_query = f'update filemanagement set {column} = %s where fileID = %s'
-        self.cursor.execute(update_query, (newvalue,fileID,))
-        self.mydb.commit()
-        
-        if self.cursor.rowcount > 0:
-            print(f'File with ID {fileID} has been updated')
-            
-        else:
-            print('No matching file')
-      
+        try:
+            fileID = int(input('Enter file ID of what to update: '))
+            column = input('Enter column to update its value: ')
+            newvalue = input('Enter the new value: ')
+            update_query = 'UPDATE filemanagement SET ' + column + ' = %s WHERE fileID = %s'
+            self.cursor.execute(update_query, (newvalue, fileID,))
+            self.mydb.commit()
+
+            if self.cursor.rowcount > 0:
+                print(f'File with ID {fileID} has been updated.')
+            else:
+                print('No matching file')
+        except mysql.connector.Error as err:
+            print(f"SQL Error: {err}")
+
     def delete_fileinformation(self):
-        fileID = input('Enter fileID to delete: ')
-        delete_query = f'delete from filemanagement where fileID = %s'
-        self.cursor.execute(delete_query,(fileID,))
-        self.mydb.commit()
-        
-        if self.cursor.rowcount > 0:
-            print(f'Property with ID {fileID} has been deleted')
-            
-        else:
-            print('No matching file')
-            
+        try:
+            fileID = int(input('Enter fileID to delete: '))
+            delete_query = 'DELETE FROM filemanagement WHERE fileID = %s'
+            self.cursor.execute(delete_query, (fileID,))
+            self.mydb.commit()
+
+            if self.cursor.rowcount > 0:
+                print(f'File with ID {fileID} has been deleted.')
+            else:
+                print('No matching file')
+        except mysql.connector.Error as err:
+            print(f"SQL Error: {err}")
+
     def display_fileinformation(self):
-        self.cursor.execute('select * from filemanagement')
-        for x in self.cursor:
-            print(x)
-            
+        try:
+            self.cursor.execute('SELECT * FROM filemanagement')
+            for x in self.cursor:
+                print(x)
+        except mysql.connector.Error as err:
+            print(f"SQL Error: {err}")
+
+    def close_connection(self):
+        self.cursor.close()
+        self.mydb.close()
+
+# Main execution
 management = FileManagement()
 admin = '1107'
 adminpassid = input('Enter Admin PassID: ')
-counter = 1
-system = input('Enter start to perform operation: ')
+
 if admin == adminpassid:
-    while system == 'start':
-        operation = input('Choose task [accept file,search file,update file,delete file,display file,logout]: ')
+    while True:
+        operation = input('Choose task [accept file, search file, update file, delete file, display file, logout]: ').lower()
         if operation == 'accept file':
             management.accept_fileinformation()
         elif operation == 'search file':
-             management.search_fileinformation()
+            management.search_fileinformation()
         elif operation == 'update file':
-             management.update_fileinformation()
+            management.update_fileinformation()
         elif operation == 'delete file':
-             management.delete_fileinformation()
+            management.delete_fileinformation()
         elif operation == 'display file':
-             management.display_fileinformation()
+            management.display_fileinformation()
         elif operation == 'logout':
-            print('shutting down app')
+            print('Shutting down app')
             break
-            system = input('Enter start to keep performing operation: ')
-            counter += 1
-            
 else:
     print('Invalid Admin Pass ID')
+
+management.close_connection()
